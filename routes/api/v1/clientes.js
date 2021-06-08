3ea6d734a97c29a851a75829c1b18727937c49f8
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2021-06-08 15:10:44 -0300

    Criando as rotas para Clientes

---
+++ routes/api/v1/clientes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+const Validation = require('express-validation');
+const auth = require('../../auth');
+
+const ClienteController = require('../../../controllers/ClienteController');
+const { LojaValidation } = require('../../../controllers/validacoes/lojaValidation');
+const { ClienteValidation } = require('../../../controllers/validacoes/clienteValidation');
+
+const clienteController = new ClienteController();
+
+// ADMIN
+router.get('/', auth.required, LojaValidation.admin, clienteController.index);
+// router.get('/search/:search/pedidos', auth.required, LojaValidation.admin, clienteController.searchPedidos);
+router.get('/search/:search', auth.required, LojaValidation.admin, clienteController.search);
+router.get('/admin/:id', auth.required, LojaValidation.admin, clienteController.showAdmin);
+// router.get('/admin/:id/:pedidos', auth.required, LojaValidation.admin, clienteController.showPedidosCliente);
+
+router.put('/admin/:id', auth.required, LojaValidation.admin, clienteController.updateAdmin);
+
+// CLIENTE
+router.get('/:id', auth.required, clienteController.show);
+
+router.post('/', clienteController.store);
+router.put('/:id', auth.required, clienteController.update);
+router.delete('/:id', auth.required, clienteController.remove);
+
+module.exports = router;
