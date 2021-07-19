e166e6e50ccdfe87b6e45485c470c7273811fbf5
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2021-07-19 10:33:10 -0300

    Criando arquivo de consfiguraçao para a integracao com a APi do Pagseguro

---
+++ config/pagseguro.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'live' : 'sandbox',
+  sandbox: process.env.NODE_ENV !== 'production',
+  sandbox_email: process.env.NODE_ENV === 'production' ? null : 'email@433BF048481D4FDDAB12D81F3AED67CC.pagseguro.com.br',
+  email: 'raphael.ohlsen@gmail.com',
+  token: '433BF048481D4FDDAB12D81F3AED67CC',
+  notificationURL: 'https://api.loja-teste.ampliee.com/v1/api/pagamentos/notificacao',
+};
